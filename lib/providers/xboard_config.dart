49373c5b3586db8da6b99f23317b3fa96bf5dbc7
rmfeel <rmemotion@gmail.com>
2025-12-12 23:18:49 +0800

    修复xboard_config.dart使用riverpod_annotation

--- lib/providers/xboard_config.dart
+++ lib/providers/xboard_config.dart
@@ -1,31 +1,37 @@
 import 'dart:convert';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fl_clash/models/xboard_config.dart';
 
-class XboardConfigNotifier extends StateNotifier<XboardConfig> {
-  XboardConfigNotifier() : super(const XboardConfig()) {
-    _loadConfig();
-  }
+part 'xboard_config.g.dart';
 
+// 默认后端地址 - 在这里修改你的 Xboard 后端地址
+const String defaultBackendUrl = 'https://cdn.98kjc.icu';
+
+@riverpod
+class XboardConfig extends _$XboardConfig {
   static const String _configKey = 'xboard_config';
-  // 默认后端地址 - 在这里修改你的 Xboard 后端地址
-  static const String defaultBackendUrl = 'https://cdn.98kjc.icu';
+
+  @override
+  models.XboardConfig build() {
+    _loadConfig();
+    return const models.XboardConfig();
+  }
 
   Future<void> _loadConfig() async {
     final prefs = await SharedPreferences.getInstance();
     final configJson = prefs.getString(_configKey);
     if (configJson != null) {
       try {
-        final config = XboardConfig.fromJson(json.decode(configJson));
+        final config = models.XboardConfig.fromJson(json.decode(configJson));
         state = config;
       } catch (e) {
         // 解析失败，使用默认地址
-        state = XboardConfig(backendUrl: defaultBackendUrl);
+        state = models.XboardConfig(backendUrl: defaultBackendUrl);
       }
     } else {
       // 首次启动，使用默认地址
-      state = XboardConfig(backendUrl: defaultBackendUrl);
+      state = models.XboardConfig(backendUrl: defaultBackendUrl);
       await _saveConfig();
     }
   }
@@ -66,8 +72,3 @@ class XboardConfigNotifier extends StateNotifier<XboardConfig> {
     await _saveConfig();
   }
 }
-
-final xboardConfigProvider =
-    StateNotifierProvider<XboardConfigNotifier, XboardConfig>((ref) {
-  return XboardConfigNotifier();
-});
